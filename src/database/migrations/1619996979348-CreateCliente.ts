93ba243a97b6b240625edd74f8f08fc3f1ae74d0
wagneernasc <wagneer_nascimento@live.com>
2021-05-03 15:37:34 -0300

    criando migrations

---
+++ src/database/migrations/1619996979348-CreateCliente.ts
@@ -0,0 +1,86 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCliente1619996979348 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'clientes',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'cnpj',
+                        type: 'varchar',
+
+                    },
+                    {
+                        name: 'razaoSocial',
+                        type: 'varchar',
+
+                    },
+                    {
+                        name: 'contato',
+                        type: 'varchar',
+
+                    },
+                    {
+                        name: 'telefone',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'endereco',
+                        type: 'varchar',
+
+                    },
+                    {
+                        name: 'numero',
+                        type: 'varchar',
+
+                    },
+                    {
+                        name: 'complemento',
+                        type: 'varchar',
+
+                    },
+                    {
+                        name: 'bairro',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'cidade',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'estado',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'cep',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                ],
+            }),
+        )
+    }
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('clientes');
+
+    }
+
+}
