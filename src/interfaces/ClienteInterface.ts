63500a0c51132cb8a74ef92b131da5afbb0a3cac
wagneernasc <wagneer_nascimento@live.com>
2021-05-03 15:36:49 -0300

    criando interfaces

---
+++ src/interfaces/ClienteInterface.ts
@@ -0,0 +1,13 @@
+export interface RequestCreateClienteData {
+    cnpj: string;
+    razaoSocial: string;
+    contato: string;
+    telefone: string;
+    endereco: string;
+    numero: string;
+    complemento: string;
+    bairro: string;
+    cidade: string;
+    estado: string;
+    cep: string;
+}
